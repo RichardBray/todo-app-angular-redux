7fca96090aeb6ec8cca91d230e47b080a1b981dc
Richardbray89 <richardobray@yahoo.co.uk>
2017-08-04 14:14:31 +0100

    feature(app.module): added middleware for chrome plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { todos } from './redux/reducer';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, StoreModule.forRoot({ todos })],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({ todos }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25 //  Retains last 25 states
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
